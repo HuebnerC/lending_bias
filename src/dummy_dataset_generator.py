6d586e13058409f76b39fd47bb8c6197ec404149
Carla Huebner <carla.c.huebner@gmail.com>
2021-05-08 14:52:54 -0600

    created Wells Fargo data preprocessing, bias evaluation document, started working on Flask app using examples

--- src/dummy_dataset_generator.py
+++ src/dummy_dataset_generator.py
@@ -1,14 +1,14 @@
 # Evaluating racial bias
 
-def racial_dummyset(processed_df):
+def racial_dummyset(processed_df, fitted_model):
     
     # Create array of predicted values on entire test set
-    y_pred_base = rf_fitted.predict(X_test)
+    y_pred_baseline = fitted_model.predict(X_test)
 
     # Make df of predictions with predicted and true columns to be used for comparison
     analysis = X_test.copy() 
-    analysis['Pred Approval'] = y_pred_base
-    analysis['Actual Approval'] = y_test
+    analysis['Pred Approval'] = y_pred_baseline
+    analysis['Actual Approval'] = y_test_baseline
     
     
     # Create DFs with indices of black applicants and white applicants, respectively. Because I am pulling records
@@ -19,28 +19,34 @@ def racial_dummyset(processed_df):
     joint_race = analysis[analysis['derived_race_Joint'] == 1]
     race_unavail = analysis[analysis['derived_race_Race Not Available'] == 1]
 
-    # Create confusion matrix for baseline type I, II error rates
-    black_conf = confusion_matrix(black[y_test_base], black[y_pred_base])
-    white_conf = confusion_matrix(white[y_test_base], white[y_pred_base])
-    asian_conf = plus_racial_conf = confusion_matrix(asian[y_test_base], asian[y_pred_base])
-    joint_race_conf = confusion_matrix(joint_race[y_test_base], joint_race[y_pred_base])
-    race_unavail_conf = confusion_matrix(race_unavail[y_test_base], race_unavail[y_pred_base])
+    # Create confusion matrix to determine baseline type I, II error rates
+    black_conf = confusion_matrix(black[y_test_baseline], black[y_pred_baseline], normalize = 'all')
+    white_conf = confusion_matrix(white[y_test_baseline], white[y_pred_baseline], normalize = 'all')
+    asian_conf = plus_racial_conf = confusion_matrix(asian[y_test_baseline], asian[y_pred_baseline], normalize = 'all')
+    joint_race_conf = confusion_matrix(joint_race[y_test_baseline], joint_race[y_pred_baseline], normalize = 'all')
+    race_unavail_conf = confusion_matrix(race_unavail[y_test_baseline], race_unavail[y_pred_baseline], normalize = 'all')
 
     
-    # Creating a copy of test set then switching the race columns for black and white applicants
-    X_test_swap = X_test.copy()
-    X_test_swap['derived_race_Black or African American'] = X_test_swap['derived_race_Black or African American'] \
-                                                                        .map(lambda x: 0 if x ==1 else 1)
-    X_test_swap['derived_race_White'] = X_test_swap['derived_race_White'] \
-                                                                    .map(lambda x: 0 if x ==1 else 1)
+    # Create a copy of processed dataset then switch the race columns for each minority group and white applicants
+    # if black == 1, change to 0 and change white to 1
+    # if white == 1, change to 0 and change black to 1
+    X_test_black = X_test.copy()
+    cond = (X_test_black['derived_race_Black or African American'] ==1) | (X_test_black['derived_race_White'] == 1)
+    true_race = 'derived_race_Black or African American'
+    white = 'derived_race_White'
+    X_test_black.loc[cond, [true_race, white]] = X_test_black.loc[cond, [white, true_race]].values
     # Predict on swapped race DF
-    swap_pred = rf_fitted.predict(X_test_swap)
+    pred_swap_black = fitted_model.predict(X_test_black)
 
     # re-rerun confusion matrix stats
-    swap_analysis = X_test_swap.copy()
-    swap_analysis['Pred Approval'] = y_pred
-    swap_analysis['Actual Approval'] = y_test
-    onfusion_matrix(y_true, y_pred)
+    black_swap_analysis = X_test_black.copy()
+    black_swap_analysis['Pred Approval'] = pred_swap_black
+    black_swap_analysis['Actual Approval'] = y_test
+    black = black_swap_analysis[black_swap_analysis['derived_race_Black or African American'] == 1]
+    white = black_swap_analysis[black_swap_analysis['derived_race_White'] == 1]
+    black_conf = confusion_matrix(black['Actual Approval'], black['Pred Approval'], normalize= 'all')
+    white_conf = confusion_matrix(white['Actual Approval'], white['Pred Approval'], normalize = 'all')
+        
 
     # Create dictionary {{black: type_1: num, type_2: num}, 
     #                     {asian: type_1}  }
